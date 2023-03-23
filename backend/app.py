b68a36f599007e5029a62c29890f31c4391700e0
Augustelli <a.mancuso@alumno.um.edu.ar>
2023-03-23 09:26:26 -0300

    funcion para crear app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
-from flask import Flask
 import os
-from dotenv import load_dotenv
-load_dotenv()
+from main import create_app
 
-app = Flask(__name__)
 
+app = create_app()
+app.app_context().push()
 if __name__ == '__main__':
     app.run(debug = True, port = os.getenv('PORT'))
---
+++ backend/install.sh
@@ -0,0 +1,3 @@
+python3 -m venv .
+source bin/active
+pip3 install -r requirements.txt
