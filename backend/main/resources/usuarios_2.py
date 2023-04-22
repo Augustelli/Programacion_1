13f7a6c2e6965c7ff3b81d75bcf0574c6b245567
Augustelli <a.mancuso@alumno.um.edu.ar>
2023-04-22 17:04:04 -0300

    Se cambiaron los ciertos recuros, ver los siguientes. Estructurabásica de los recursos montada

--- backend/main/resources/usuarios_2.py
+++ backend/main/resources/usuarios_2.py
@@ -1,62 +1,77 @@
 from flask_restful import Resource
 from flask import request, abort, jsonify
 from main import db
-from main.models import UsuarioModelo, ProfesorModelo
+from main.models import UsuarioModelo
+from sqlalchemy import or_
 
 
 class Usuarios(Resource):
 
     # Como aplicar roles
-
+    # Devolver listado de alumnos
     def get(self):
+
         try:
-            query = db.session.query(UsuarioModelo).all()
-            return query.to_json()
+            usuarios = db.session.usuario(UsuarioModelo).all()
+            return jsonify(usuario.to_json() for usuario in usuarios)
 
         except Exception:
             abort(404, 'Query no encontrado')
+        finally:
+            db.session.close()
 
     def post(self):
-        '''POST -> Rol admin'''
+        # Crear un usuario
         try:
-            informacion = request.get_json()
-            id = str(int(max(datos.keys())) + 1)
-            datos[id] = informacion
-            return datos[id], 201
+            usuario_nuevo = UsuarioModelo.from_json(request.get_json())
+            db.session.add(usuario_nuevo)
+            db.session.commit()
+            return usuario_nuevo.to_json(), 201
 
         except BaseException:
             abort(404, 'Error al crear el usuario')
+        finally:
+            db.session.close()
 
 
 class Usuario(Resource):
 
     def get(self, user_id):
-      try:
-          if datos[str(user_id)]:
-              return datos[(user_id)]
-      except BaseException:
-          abort(404, '')
+        try:
+            usuario_rescatado = db.session.query(UsuarioModelo).filter(
+                UsuarioModelo.idUsuario == user_id,).first()
+            return usuario_rescatado.to_json(), 201
+        except Exception:
+            abort(404, f'No se ha encontrado del usuario de id: {user_id}')
+        finally:
+            db.session.close()
 
     def put(self, user_id):
-        '''PUT -> Rol Admin'''
+
         try:
-            if str(user_id) in datos.keys():
-                usuario = datos[str(user_id)]
-                data = request.get_json()
-                usuario.update(data)
-                return 'Información actualizada con éxito', 201
+            usuario_editar = db.session.query(UsuarioModelo).filter(UsuarioModelo.idUsuario == user_id).first()
+            informacion = request.get_json().items()
+            for campo, valor in informacion:
+                setattr(usuario_editar, campo, valor)
+            db.session.add(usuario_editar)
+            db.session.commit()
+            return usuario_editar.to_json(), 201
         except BaseException:
             abort(404, 'No se ha podido actualizar el usuario de id {}'.format(user_id))
+        finally:
+            db.session.close()
 
     def delete(self, user_id):
-        '''DELETE -> Rol Admin'''
 
         try:
-            if datos[user_id]:
-                del datos[(user_id)]
-                return ('Usuario de id {} eliminado'.format(user_id), 204)
+            usuario_eliminar = db.session.query(UsuarioModelo).filter(UsuarioModelo.idUsuario == user_id).first()
+            db.session.delete(usuario_eliminar)
+            db.session.commit()
+            return 204
         except BaseException:
             abort(404, 'No se ha encontrado el usuario de id {}'.format(user_id))
+        finally:
+            db.session.close()
 
 
 class UsuariosAlumnos(Resource):
